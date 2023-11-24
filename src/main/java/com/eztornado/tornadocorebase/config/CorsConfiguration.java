288b3979eab412f3cc718ff252ffb29565100a7c
eztornado <ivanps94@protonmail.com>
2023-11-24 18:34:57 +0100

    Update application.properties. Enable Cors

---
+++ src/main/java/com/eztornado/tornadocorebase/config/CorsConfiguration.java
@@ -0,0 +1,31 @@
+package com.eztornado.tornadocorebase.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+
+                registry.addMapping("/api/auth/**") // or specify a path pattern for more granular control
+                        .allowedOrigins("*") // the front-end server address
+                        .allowedMethods("GET", "POST", "PUT", "DELETE") // allowed HTTP methods
+                        .allowedHeaders("*") // allowed headers
+                        .allowCredentials(false); // if you need credentials
+
+                registry.addMapping("/api/**") // or specify a path pattern for more granular control
+                        .allowedOrigins("*") // the front-end server address
+                        .allowedMethods("GET", "POST", "PUT", "DELETE") // allowed HTTP methods
+                        .allowedHeaders("*") // allowed headers
+                        .allowCredentials(true); // if you need credentials
+            }
+        };
+    }
+}
